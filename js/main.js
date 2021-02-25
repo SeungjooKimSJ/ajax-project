bd19debaba2db2c4a07e64bfcbcc22ea77213cad
Seungjoo Kim <sjskybone@gmail.com>
2021-02-24 20:40:52 -0800

    Styling for the search page. #1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-var $searchIcon = document.querySelector('.header-search-icon');
-var $mainFirstBtn = document.querySelector('.main-first-btn');
+// var $formSearch = document.createElement('form');
+// var $label = document.createElement('label');
+// var $labelInput = document.createElement('input');
 
-function clickSearch(event) {
-  var $greeting = document.querySelector('.greeting');
-  $greeting.textContent = 'Where are your places';
-};
+// $labelInput.setAttribute('type', 'search');
+// $labelInput.setAttribute('placeholder', 'search bucket places');
 
-$searchIcon.addEventListener('click', clickSearch);
-$mainFirstBtn.addEventListener('click', clickSearch);
+// var $header = document.querySelector('header');
+// $formSearch.appendChild($label);
+// $header.appendChild($formSearch);
+
+// var $greeting = document.querySelector('.greeting');
+// $greeting.textContent = 'Where are your places';
+
+
+// var $searchIcon = document.querySelector('.header-search-icon');
+// var $mainFirstBtn = document.querySelector('.main-first-btn');
+
+// function clickSearch(event) {
+//   var $greeting = document.querySelector('.greeting');
+//   $greeting.textContent = 'Where are your places';
+// };
+
+// $searchIcon.addEventListener('click', clickSearch);
+// $mainFirstBtn.addEventListener('click', clickSearch);
 
 
 // function searchPlaces(event) {
-//   var $formSearch = document.createElement('form');
-//   var $label = document.createElement('label');
 
-//   var $labelInput = document.createElement('input');
-//   $labelInput.setAttribute('type', 'search');
-//   $labelInput.setAttribute('placeholder', 'search bucket places');
-// }
+// };
+
+// $searchIcon.addEventListener('click', searchPlaces);
+// $mainFirstBtn.addEventListener('click', searchPlaces);
